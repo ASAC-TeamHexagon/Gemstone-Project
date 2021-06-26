41f0c0e66647c0153a9d441ac1e28b2e664285d3
Ali-coder1996 <alisalamehhjouj@gmail.com>
2021-06-27 00:35:50 +0300

    add css in form and about us

--- js/form.js
+++ js/form.js
@@ -1,13 +1,12 @@
 'use strict';
-// let form = document.getElementById('form');
+let form = document.getElementById('form');
 // console.log(form);
 
-function Gemstone (first_name,last_name,Email,password,area_code,phone,subject){
+function Gemstone (first_name,last_name,Email,password,phone,subject){
     this.first_name = first_name;
     this.last_name = last_name;
     this.Email = Email;
     this.password = password;
-    this.area_code = area_code;
     this.phone = phone;
     this.subject = subject;
     Gemstone.all.push(this);
@@ -16,13 +15,12 @@ Gemstone.all = [];
 
 function addNewUser(e) {
 
-    // e.preventDefault();
+    e.preventDefault();
     
 let first_name = event.target.first_name.value;
   let last_name = event.target.last_name.value;
   let Email = event.target.Email.value;
   let password = event.target.password.value;
-  let area_code = event.target.area_code.value;
   let phone = event.target.phone.value;
   let subject = event.target.subject.value;
   alert(`Hello ${first_name} ${last_name}, registration confirmed, please return to home Page to serve you !!! `);
