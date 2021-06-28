2cab607007514938b46029b7839a55af49ae804b
Ali-coder1996 <alisalamehhjouj@gmail.com>
2021-06-28 09:04:39 +0300

    add cart

--- js/products.js
+++ js/products.js
@@ -10,10 +10,11 @@ function ProductAdd(name, price, image, category, description) {
     this.image = image;
     this.category = category;
     this.description = description;
+    this.inCart = 0;
 
-
+    ProductAdd.all.push(this);
 }
-
+ProductAdd.all = [];
 
 ProductAdd.prototype.render = function () {
     let container = document.createElement('div');
@@ -48,7 +49,7 @@ ProductAdd.prototype.render = function () {
     let description = document.createElement('p');
     details.appendChild(description);
     description.textContent = this.description;
-    
+
 
     let productInfo = document.createElement('div');
     productInfo.className = "product-bottom-details";
@@ -79,13 +80,32 @@ ProductAdd.prototype.render = function () {
 
 }
 
-let amber = new ProductAdd('Amber Stone', 260 + '$', './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
-let emeraldRing = new ProductAdd('Emerald Ring', 1000 +'$', './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+let amber = new ProductAdd('Amber Stone', 260, './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+let emeraldRing = new ProductAdd('Emerald Ring', 1000, './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+
+let amber2 = new ProductAdd('Amber Stone', 260 , './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+let emeraldRing2 = new ProductAdd('Emerald Ring', 1000 , './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+
+
+let amber3 = new ProductAdd('Amber Stone', 260 , './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+let emeraldRing3 = new ProductAdd('Emerald Ring', 1000 , './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+
+
+
+
+
 
 
 
 amber.render();
 emeraldRing.render();
+
+amber2.render();
+emeraldRing2.render();
+amber3.render();
+emeraldRing3.render();
+
+
 
 
 function formSubmission(event) {
@@ -99,11 +119,89 @@ function formSubmission(event) {
 
 
 
-    let newProductAdd = new ProductAdd(name, price, image, category, description );
+    let newProductAdd = new ProductAdd(name, price, image, category, description);
     newProductAdd.render();
-    alert('Your product added successfully ')
+    swal("Good job!", "Your submit done!", "success");
+    // for (let i = 0; i < ProductAdd.length; i++) {
+    //     console.log(ProductAdd.all[i]);
 
-    
+    // }
+
+
+}
+gemStoneForm.addEventListener('submit', formSubmission);
+
+
+
+let carts = document.querySelectorAll('.fa-shopping-bag');
+
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        // console.log(ProductAdd.all.name);
+
+        cartNumbers(ProductAdd.all[i]);
+        totalCost(ProductAdd.all[i]);
+        console.log(ProductAdd.all);
+    }
+    )
 }
 
-gemStoneForm.addEventListener('submit', formSubmission);
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    if (productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+
+function cartNumbers(pro) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+    setItem(pro);
+}
+
+
+function setItem(pro) {
+    let cartItem = localStorage.getItem('productsInCart');
+    cartItem = JSON.parse(cartItem);
+    if (cartItem != null) {
+        if (cartItem[pro.name] == undefined) {
+            cartItem = {
+                ...cartItem,
+                [pro.name]: pro
+            }
+        }
+        cartItem[pro.name].inCart += 1;
+    } else {
+        pro.inCart = 1;
+        cartItem = {
+            [pro.name]: pro
+        }
+    }
+
+    localStorage.setItem('productsInCart', JSON.stringify(cartItem));
+}
+
+
+function totalCost(pro) {
+    let cartCost = localStorage.getItem('totalCost');
+    // console.log(ProductAdd.all.price);
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem('totalCost', cartCost + pro.price );
+
+    } else {
+        localStorage.setItem('totalCost', pro.price);
+    }
+}
+
+onLoadCartNumbers();
