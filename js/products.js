32214a73de4ec27219651534ccc191a45507ee15
Thaer AlJomhawi <thaer.fjomhawi@gmail.com>
2021-06-28 17:28:47 +0300

    update the product cards add rateing

--- js/products.js
+++ js/products.js
@@ -16,7 +16,7 @@ function ProductAdd(name, price, image, category, description) {
 }
 ProductAdd.all = [];
 
-ProductAdd.prototype.render = function() {
+ProductAdd.prototype.render = function () {
     let container = document.createElement('div');
     container.className = "product-card";
     products.appendChild(container);
@@ -78,17 +78,54 @@ ProductAdd.prototype.render = function() {
     cartIcon.className = "fas fa-shopping-bag"
     cart.appendChild(cartIcon);
 
+    let rate = document.createElement('div')
+    rate.className = "rating"
+    productLinks.appendChild(rate);
+
+    for (let i =0; i < Math.floor(Math.random() * 5) + 1;i++)
+    {
+        let stars= document.createElement('i');
+        stars.className = "rating__star fas fa-star";
+        rate.appendChild(stars);
+    
+    }
+    
 }
 
-let amber = new ProductAdd('Amber Stone', 260 +"$", './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
-let emeraldRing = new ProductAdd('Emerald Ring', 1000 +"$", './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
 
-let amber2 = new ProductAdd('Amber Stone', 210+"$", './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
-let emeraldRing2 = new ProductAdd('Emerald Ring', 1100 +"$", './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+let ratingStars = [...document.getElementsByClassName("rating__star")];
+
+function executeRating(stars) {
+    const starClassActive = "rateStar fas fa-star";
+    const starClassInactive = "rateStar far fa-star";
+    const starsLength = stars.length;
+    let i;
+    stars.map((star) => {
+        star.onclick = () => {
+            i = stars.indexOf(star);
+
+            if (star.className === starClassInactive) {
+                for (i; i >= 0; --i) stars[i].className = starClassActive;
+            } else {
+                for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+            }
+        };
+    });
+}
+
+executeRating(ratingStars);
 
 
-let amber3 = new ProductAdd('Amber Stone', 220+"$", './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
-let emeraldRing3 = new ProductAdd('Emerald Ring', 1500+"$", './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+
+let amber = new ProductAdd('Amber Stone', 260 + "₿", './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+let emeraldRing = new ProductAdd('Emerald Ring', 1000 + "₿", './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+
+let amber2 = new ProductAdd('Amber Stone', 210 + "₿", './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+let emeraldRing2 = new ProductAdd('Emerald Ring', 1100 + "₿", './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+
+
+let amber3 = new ProductAdd('Amber Stone', 220 + "₿", './img/amber.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
+let emeraldRing3 = new ProductAdd('Emerald Ring', 1500 + "₿", './img/Emerald-ring.png', 'Gemstones', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!');
 
 
 
@@ -119,7 +156,7 @@ function formSubmission(event) {
 
 
 
-    let newProductAdd = new ProductAdd(name, price +"$", image, category, description);
+    let newProductAdd = new ProductAdd(name, price + "₿", image, category, description);
     newProductAdd.render();
     swal("Good job!", "Your submit done!", "success");
     // for (let i = 0; i < ProductAdd.length; i++) {
@@ -208,7 +245,7 @@ onLoadCartNumbers();
 
 
 
-    
+
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -218,18 +255,18 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn.onclick = function () {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
