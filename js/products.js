fa6900546f1f7bc5d2ebe7ede8529071168672d2
Ali-coder1996 <alisalamehhjouj@gmail.com>
2021-06-29 20:48:46 +0300

    fix

--- js/products.js
+++ js/products.js
@@ -6,7 +6,7 @@ let products = document.getElementById('products');
 
 function ProductAdd(name, price, image, category, description) {
     this.name = name;
-    this.price = price + '  ₿';
+    this.price = price;
     this.image = image;
     this.category = category;
     this.description = description;
